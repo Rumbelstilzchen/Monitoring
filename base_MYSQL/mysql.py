05e647c1812d220a4f9fe9255b169e8c46dad873
Rumbelstilzchen <rumbelstilzchen@yahoo.de>
2020-10-04 21:23:28 +0200

    added handling of "update" tag for DWD monitoring

--- base_MYSQL/mysql.py
+++ base_MYSQL/mysql.py
@@ -12,6 +12,10 @@ class db_write:
 
     def __init__(self, config):
         self.config = config
+        if 'StatementType' in self.config.keys():
+            self.statement_type = self.config['StatementType']
+        else:
+            self.statement_type = 'Insert'
         self.connect()
 
     def __del__(self):
@@ -37,16 +41,64 @@ class db_write:
 
     def write_dict_data(self, dictionary):
         myDict = OrderedDict(dictionary)
+        if isinstance(myDict['TIMESTAMP'], list):
+            success_state = self.write_dict_data_multiple(myDict)
+        else:
+            success_state = self.write_dict_data_single(myDict)
+        return success_state
+
+    def write_dict_data_multiple(self, myDict):
+        if self.statement_type.upper() == 'UPDATE':
+            try:
+                if 'time_sec' in myDict.keys():
+                    time_column = 'time_sec'
+                elif 'TIMESTAMP' in myDict.keys():
+                    time_column = 'TIMESTAMP'
+                else:
+                    logger.error('Cannot Update Entries - eleteing is not possible as neither time_sec nor TIMESTAMP is found')
+                    raise NotImplementedError
+                with self.connection.cursor() as cursor:
+                    sql_delete = 'DELETE FROM %s WHERE %s >= %%s' % (self.config['mysql_table'], time_column)
+                    cursor.execute(sql_delete, myDict[time_column][0])
+                    result = self.connection.commit()
+                    if result is not None:
+                        logger.warning('MYSQL_return: %s' % str(result))
+                logger.debug("MYSQL: deleting old entries")
+                success = True
+            except Exception:
+                logger.exception('MYSQLERROR')
+        for index in range(len(myDict['TIMESTAMP'])):
+            try:
+                with self.connection.cursor() as cursor:
+
+                    placeholders = ', '.join(['%s'] * len(myDict))
+                    columns = ', '.join(myDict.keys())
+                    sql = "INSERT INTO %s ( %s ) VALUES ( %s )" % (self.config['mysql_table'], columns, placeholders)
+                    cursor.execute(sql, [x[index] for x in myDict.values()])
+                    result = self.connection.commit()
+                    if result is not None:
+                        logger.warning('MYSQL_return: %s' % str(result))
+                logger.debug("MYSQL: %s", str([x[index] for x in myDict.values()]))
+                success = True
+            except Exception:
+                logger.exception('MYSQLERROR')
+                success = False
+        return success
+
+    def write_dict_data_single(self, myDict):
+        # if self.statement_type.upper() == 'UPDATE':
+        #     # delete old entries
+        #     pass
         try:
-            # with self.connection.cursor() as cursor:
-            #     placeholders = ', '.join(['%s'] * len(myDict))
-            #     columns = ', '.join(myDict.keys())
-            #     sql = "INSERT INTO %s ( %s ) VALUES ( %s )" % (self.config['mysql_table'], columns, placeholders)
-            #     cursor.execute(sql, [x for x in myDict.values()])
-            #     result = self.connection.commit()
-            #     if result is not None:
-            #         logger.warning('MYSQL_return: %s' % str(result))
-            logger.info("MYSQL: %s", str([x for x in myDict.values()]))
+            with self.connection.cursor() as cursor:
+                placeholders = ', '.join(['%s'] * len(myDict))
+                columns = ', '.join(myDict.keys())
+                sql = "INSERT INTO %s ( %s ) VALUES ( %s )" % (self.config['mysql_table'], columns, placeholders)
+                cursor.execute(sql, [x for x in myDict.values()])
+                result = self.connection.commit()
+                if result is not None:
+                    logger.warning('MYSQL_return: %s' % str(result))
+            logger.debug("MYSQL: %s", str([x for x in myDict.values()]))
             success = True
         except Exception:
             logger.exception('MYSQLERROR')
