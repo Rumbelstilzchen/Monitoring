e5d5426a61cf3823e9494cd076d618228204f697
Rumbelstilzchen <rumbelstilzchen@yahoo.de>
2020-10-05 23:10:31 +0200

    no uses mysql-connector-python instead of PyMySQL as it has support for setting the time_one on per session basis

--- base_MYSQL/mysql.py
+++ base_MYSQL/mysql.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import logging
-import pymysql as mysql
+# import pymysql as mysql
+import mysql.connector as mysql
 from collections import OrderedDict
 logger = logging.getLogger(__name__)
 # logger = logging.getLogger()
@@ -28,7 +29,19 @@ class db_write:
                                             user=self.config['mysql_username'],
                                             password=self.config['mysql_pw'],
                                             db=self.config['mysql_DB'],
-                                            charset='utf8mb4')
+                                            charset='utf8')
+            if 'time_zone' in self.config.keys():
+                self.connection.time_zone = self.config['time_zone']
+            else:
+                self.connection.time_zone = 'Europe/Berlin'
+
+            if self.connection.is_connected():
+                db_Info = self.connection.get_server_info()
+                logger.info("Connected to MySQL Server version %s", db_Info)
+                with self.connection.cursor() as cursor:
+                    cursor.execute("select database();")
+                    record = cursor.fetchone()
+                    logger.info("You're connected to database: %s", record)
         except Exception as e:
             logger.exception('Mysql cannot connect')
             raise e
@@ -48,41 +61,42 @@ class db_write:
         return success_state
 
     def write_dict_data_multiple(self, myDict):
+        success = True
+        if 'time_sec' in myDict.keys():
+            time_column = 'time_sec'
+        elif 'TIMESTAMP' in myDict.keys():
+            time_column = 'TIMESTAMP'
+        else:
+            logger.error('Cannot Update Entries - deleteing is not possible as neither time_sec nor TIMESTAMP is found')
+            raise NotImplementedError
         if self.statement_type.upper() == 'UPDATE':
             try:
-                if 'time_sec' in myDict.keys():
-                    time_column = 'time_sec'
-                elif 'TIMESTAMP' in myDict.keys():
-                    time_column = 'TIMESTAMP'
-                else:
-                    logger.error('Cannot Update Entries - eleteing is not possible as neither time_sec nor TIMESTAMP is found')
-                    raise NotImplementedError
                 with self.connection.cursor() as cursor:
-                    sql_delete = 'DELETE FROM %s WHERE %s >= %%s' % (self.config['mysql_table'], time_column)
-                    cursor.execute(sql_delete, myDict[time_column][0])
-                    result = self.connection.commit()
-                    if result is not None:
-                        logger.warning('MYSQL_return: %s' % str(result))
+                    sql_delete = 'DELETE FROM %s WHERE %s >= %s' % (self.config['mysql_table'], time_column, myDict[time_column][0])
+                    cursor.execute(sql_delete)
+                    self.connection.commit()
                 logger.debug("MYSQL: deleting old entries")
                 success = True
             except Exception:
-                logger.exception('MYSQLERROR')
-        for index in range(len(myDict['TIMESTAMP'])):
-            try:
-                with self.connection.cursor() as cursor:
-
-                    placeholders = ', '.join(['%s'] * len(myDict))
-                    columns = ', '.join(myDict.keys())
-                    sql = "INSERT INTO %s ( %s ) VALUES ( %s )" % (self.config['mysql_table'], columns, placeholders)
-                    cursor.execute(sql, [x[index] for x in myDict.values()])
-                    result = self.connection.commit()
-                    if result is not None:
-                        logger.warning('MYSQL_return: %s' % str(result))
-                logger.debug("MYSQL: %s", str([x[index] for x in myDict.values()]))
-                success = True
-            except Exception:
-                logger.exception('MYSQLERROR')
                 success = False
+                logger.exception('MYSQLERROR')
+        try:
+            placeholders = ', '.join(['%s'] * len(myDict))
+            columns = ', '.join(myDict.keys())
+            sql = "INSERT INTO %s ( %s ) VALUES ( %s )" % (self.config['mysql_table'], columns, placeholders)
+            entry_list = []
+            for index in range(len(myDict[time_column])):
+                entry_list.append(tuple([x[index] for x in myDict.values()]))
+            with self.connection.cursor() as cursor:
+                cursor.executemany(sql, entry_list)
+                # for index in range(len(myDict[time_column])):
+                #     cursor.execute(sql, [x[index] for x in myDict.values()])
+                self.connection.commit()
+                # logger.debug("MYSQL: %s", str([x[index] for x in myDict.values()]))
+                logger.debug("MYSQL: first element: %s", str(entry_list[0]))
+        except Exception:
+            logger.exception('MYSQLERROR')
+            success = False
         return success
 
     def write_dict_data_single(self, myDict):
@@ -95,9 +109,7 @@ class db_write:
                 columns = ', '.join(myDict.keys())
                 sql = "INSERT INTO %s ( %s ) VALUES ( %s )" % (self.config['mysql_table'], columns, placeholders)
                 cursor.execute(sql, [x for x in myDict.values()])
-                result = self.connection.commit()
-                if result is not None:
-                    logger.warning('MYSQL_return: %s' % str(result))
+                self.connection.commit()
             logger.debug("MYSQL: %s", str([x for x in myDict.values()]))
             success = True
         except Exception:
