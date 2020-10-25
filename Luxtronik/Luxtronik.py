7d0d8e5aa2ce34cadfea5b1e650dd0eb8c309f9f
Rumbelstilzchen <rumbelstilzchen@yahoo.de>
2020-10-26 00:23:03 +0100

    added logging of heating

---
+++ Luxtronik/Luxtronik.py
@@ -0,0 +1,179 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+import pytz
+import socket
+import struct
+from retry import retry
+import logging
+from urllib.request import urlopen
+import re
+
+logger = logging.getLogger(__name__)
+
+
+def check_values_empty(dict_data):
+    for value in dict_data.values():
+        if value == "":
+            return True
+    return False
+
+
+class Luxtronik:
+    name = 'Luxtronik'
+
+    def __init__(self, config):
+        self.configuration = config
+        self.hostHeatpump = self.configuration[self.name]['host']
+        self.portHeatpump = self.configuration.getint(self.name,'port')
+        if self.configuration.has_option(self.name, 'host_HK'):
+            self.tasmota = self.configuration[self.name]['host_HK']
+        else:
+            self.tasmota = None
+        self.timestamp = None
+        # self.time_zone = 'Europe/Berlin'
+        self.time_zone = 'UTC'
+        self.tz = pytz.timezone(self.time_zone)
+        self.parsed_data = {}
+        self._socket = None
+        self.tasmota_regex = r"[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?"
+
+        # self.IDs_all = ['?', '?', '?', '?', '?', '?', '?', '?', '?', '?', 'Temperatur_TVL', 'Temperatur_TRL', 'Sollwert_TRL_HZ', 'Temperatur_TRL_ext', 'Temperatur_THG', 'Temperatur_TA', 'Mitteltemperatur', 'Temperatur_TBW', 'Einst_BWS_akt', 'Temperatur_TWE', 'Temperatur_TWA', 'Temperatur_TFB1', 'Sollwert_TVL_MK1', 'Temperatur_RFV', 'Temperatur_TFB2', 'Sollwert_TVL_MK2', 'Temperatur_TSK', 'Temperatur_TSS', 'Temperatur_TEE', 'ASDin', 'BWTin', 'EVUin', 'HDin', 'MOTin', 'NDin', 'PEXin', 'SWTin', 'AVout', 'BUPout', 'HUPout', 'MA1out', 'MZ1out', 'VENout', 'VBOout', 'VD1out', 'VD2out', 'ZIPout', 'ZUPout', 'ZW1out', 'ZW2SSTout', 'ZW3SSTout', 'FP2out', 'SLPout', 'SUPout', 'MZ2out', 'MA2out', 'Zaehler_BetrZeitVD1', 'Zaehler_BetrZeitImpVD1', 'Zaehler_BetrZeitVD2', 'Zaehler_BetrZeitImpVD2', 'Zaehler_BetrZeitZWE1', 'Zaehler_BetrZeitZWE2', 'Zaehler_BetrZeitZWE3', 'Zaehler_BetrZeitWP', 'Zaehler_BetrZeitHz', 'Zaehler_BetrZeitBW', 'Zaehler_BetrZeitKue', 'Time_WPein_akt', 'Time_ZWE1_akt', 'Time_ZWE2_akt', 'Timer_EinschVerz', 'Time_SSPAUS_akt', 'Time_SSPEIN_akt', 'Time_VDStd_akt', 'Time_HRM_akt', 'Time_HRW_akt', 'Time_LGS_akt', 'Time_SBW_akt', 'Code_WP_akt', 'BIV_Stufe_akt', 'WP_BZ_akt', 'SoftStand1', 'SoftStand2', 'SoftStand3', 'SoftStand4', 'SoftStand5', 'SoftStand6', 'SoftStand7', 'SoftStand8', 'SoftStand9', 'SoftStand10', 'AdresseIP_akt', 'SubNetMask_akt', 'Add_Broadcast', 'Add_StdGateway', 'ERROR_Time0', 'ERROR_Time1', 'ERROR_Time2', 'ERROR_Time3', 'ERROR_Time4', 'ERROR_Nr0', 'ERROR_Nr1', 'ERROR_Nr2', 'ERROR_Nr3', 'ERROR_Nr4', 'AnzahlFehlerInSpeicher', 'Switchoff_file_Nr0', 'Switchoff_file_Nr1', 'Switchoff_file_Nr2', 'Switchoff_file_Nr3', 'Switchoff_file_Nr4', 'Switchoff_file_Time0', 'Switchoff_file_Time1', 'Switchoff_file_Time2', 'Switchoff_file_Time3', 'Switchoff_file_Time4', 'Comfort_exists', 'HauptMenuStatus_Zeile1', 'HauptMenuStatus_Zeile2', 'HauptMenuStatus_Zeile3', 'HauptMenuStatus_Zeit', 'HauptMenuAHP_Stufe', 'HauptMenuAHP_Temp', 'HauptMenuAHP_Zeit', 'SH_BWW', 'SH_HZ', 'SH_MK1', 'SH_MK2', 'Einst_Kurzprogramm', 'StatusSlave_1', 'StatusSlave_2', 'StatusSlave_3', 'StatusSlave_4', 'StatusSlave_5', 'AktuelleTimeStamp', 'SH_MK3', 'Sollwert_TVL_MK3', 'Temperatur_TFB3', 'MZ3out', 'MA3out', 'FP3out', 'Time_AbtIn', 'Temperatur_RFV2', 'Temperatur_RFV3', 'SH_SW', 'Zaehler_BetrZeitSW', 'FreigabKuehl', 'AnalogIn', 'SonderZeichen', 'SH_ZIP', 'WebsrvProgrammWerteBeobachten', 'WMZ_Heizung', 'WMZ_Brauchwasser', 'WMZ_Schwimmbad', 'WMZ_Seit', 'WMZ_Durchfluss', 'AnalogOut1', 'AnalogOut2', 'Time_Heissgas', 'Temp_Lueftung_Zuluft', 'Temp_Lueftung_Abluft', 'Zaehler_BetrZeitSolar', 'AnalogOut3', 'AnalogOut4', 'Out_VZU', 'Out_VAB', 'Out_VSK', 'Out_FRH', 'AnalogIn2', 'AnalogIn3', 'SAXin', 'SPLin', 'Compact_exists', 'Durchfluss_WQ', 'LIN_exists', 'LIN_ANSAUG_VERDAMPFER', 'LIN_ANSAUG_VERDICHTER', 'LIN_VDH', 'LIN_UH', 'LIN_UH_Soll', 'LIN_HD', 'LIN_ND', 'LIN_VDH_out', 'HZIO_PWM', 'HZIO_VEN', 'HZIO_EVU2', 'HZIO_STB', 'SEC_Qh_Soll', 'SEC_Qh_Ist', 'SEC_TVL_Soll', 'SEC_Software', 'SEC_BZ', 'SEC_VWV', 'SEC_VD', 'SEC_VerdEVI', 'SEC_AnsEVI', 'SEC_UEH_EVI', 'SEC_UEH_EVI_S', 'SEC_KondTemp', 'SEC_FlussigEx', 'SEC_UK_EEV', 'SEC_EVI_Druck', 'SEC_U_Inv', 'Temperatur_THG_2', 'Temperatur_TWE_2', 'LIN_ANSAUG_VERDAMPFER_2', 'LIN_ANSAUG_VERDICHTER_2', 'LIN_VDH_2', 'LIN_UH_2', 'LIN_UH_Soll_2', 'LIN_HD_2', 'LIN_ND_2', 'HDin_2', 'AVout_2', 'VBOout_2', 'VD1out_2', 'LIN_VDH_out_2', 'Switchoff2_Nr0', 'Switchoff2_Nr1', 'Switchoff2_Nr2', 'Switchoff2_Nr3', 'Switchoff2_Nr4', 'Switchoff2_Time0', 'Switchoff2_Time1', 'Switchoff2_Time2', 'Switchoff2_Time3', 'Switchoff2_Time4', 'RBE_RT_Ist', 'RBE_RT_Soll', 'Temp_BW_oben', 'Code_WP_akt_2', 'Freq_VD', 'LIN_Temp_ND', 'LIN_Temp_HD', 'Abtauwunsch', 'Abtauwunsch_2', 'Freq_Soll', 'Freq_Min', 'Freq_Max', 'VBO_Soll', 'VBO_Ist', 'HZUP_PWM', 'HZUP_Soll', 'HZUP_Ist', 'Temperatur_VLMax', 'Temperatur_VLMax_2', 'SEC_EVi', 'SEC_EEV', 'Time_ZWE3_akt']
+
+        self.IDs = {
+            10: ['flowTemperature', 0.1],
+            11: ['returnTemperature', 0.1],
+            12: ['returnTemperatureTarget', 0.1],
+            13: ['returnTemperatureExtern', 0.1],
+            15: ['ambientTemperature', 0.1],
+            16: ['averageAmbientTemperature', 0.1],
+            17: ['hotWaterTemperature', 0.1],
+            18: ['hotWaterTemperatureTarget', 0.1],
+            19: ["heatSourceIN", 0.1],
+            20: ["heatSourceOUT", 0.1],
+            44: ['compressor1', None],
+            39: ['heatingSystemCircPump', None],
+            38: ['hotWaterSwitchingValve', None],
+            151: ["counterHeatQHeating", 0.1],
+            152: ["counterHeatQHotWater", 0.1],
+            155: ["flowRate", None],
+        }
+        self.average_ignores = [
+            'TIMESTAMP',
+            'time_sec',
+            'operatingStatus',
+            'ChargeCycles',
+            'BatCurrentDir',
+            'MaxVolPos',
+            'MinVolPos',
+            'MaxTempPos',
+            'MinTempPos',
+            'CycleCounts'
+        ]
+
+    def collect_data(self):
+        self.load_data()
+        if self.tasmota is not None:
+            self.read_temp_sensor()
+        self.add_extra_entries()
+        return self.parsed_data
+
+    def _read_from_socket(self):
+        data_raw = []
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.connect((self.hostHeatpump, self.portHeatpump))
+        self._socket.sendall(struct.pack(">ii", 3004, 0))
+        cmd = struct.unpack(">i", self._socket.recv(4))[0]
+        logger.debug("Command %s", cmd)
+        stat = struct.unpack(">i", self._socket.recv(4))[0]
+        logger.debug("Stat %s", stat)
+        length = struct.unpack(">i", self._socket.recv(4))[0]
+        self.timestamp = datetime.now(self.tz)
+        logger.debug("Length %s", length)
+        for d in range(0, length):
+            data_raw.append(struct.unpack(">i", self._socket.recv(4))[0])
+        logger.debug("Read %d calculations", length)
+        self._socket.close()
+        logger.debug("Disconnected from Luxtronik heatpump  %s:%s", self.hostHeatpump, self.portHeatpump)
+        return data_raw
+
+    @retry(tries=2, delay=0)
+    def load_data(self):
+        raw_data = self._read_from_socket()
+        self.parsed_data = {}
+        for key, value in self.IDs.items():
+            if value[0] == 'flowRate':
+                flowrate = raw_data[key]
+                self.parsed_data['thermalPower'] = \
+                    abs(self.parsed_data['flowTemperature'] - self.parsed_data['returnTemperature']) * flowrate / 866.65
+            else:
+                if value[1] is not None:
+                    self.parsed_data[value[0]] = round(raw_data[key] * value[1], 1)
+                else:
+                    self.parsed_data[value[0]] = raw_data[key]
+        self.parsed_data['counterHeatQTotal'] = \
+            self.parsed_data['counterHeatQHeating'] + self.parsed_data['counterHeatQHotWater']
+
+    def read_temp_sensor(self):
+        link = "http://%s/?m=1" % self.tasmota
+        f = urlopen(link)
+        myfile = str(f.read()).split('{m}')
+        self.parsed_data['VL_HK'] = re.findall(self.tasmota_regex, myfile[1])[0]
+        self.parsed_data['RL_HK'] = re.findall(self.tasmota_regex, myfile[2])[0]
+
+    def add_extra_entries(self):
+        self.parsed_data['time_sec'] = int(self.timestamp.timestamp())
+        self.parsed_data['TIMESTAMP'] = datetime.fromtimestamp(self.parsed_data['time_sec'], self.tz).strftime(
+            '%Y-%m-%d %H:%M:%S')
+
+
+# if __name__ == "__main__":
+#     logger.error('logger file needs to be run directly')
+
+def read_data():
+    parser = Luxtronik
+
+    # from base_MYSQL.mysql import db_write
+    import configparser
+    configuration = configparser.ConfigParser()
+    configuration.sections()
+    configuration.read(r'..\config.ini')
+
+    parser_init = parser(configuration)
+
+    # MYSQL_config = {
+    #     'mysql_host': configuration['MYSQL']['mysql_host'],
+    #     'mysql_port': configuration.getint('MYSQL', 'mysql_port'),
+    #     'mysql_username': configuration[parser.name]['mysql_username'],
+    #     'mysql_pw': configuration[parser.name]['mysql_pw'],
+    #     'mysql_DB': configuration[parser.name]['mysql_DB'],
+    #     'mysql_table': configuration[parser.name]['mysql_tablename'],
+    #     'time_zone': parser_init.time_zone,
+    # }
+    # if 'mysql_type' in configuration[parser.name].keys():
+    #     MYSQL_config['StatementType'] = configuration[parser.name]['mysql_type']
+    # MYsqlConnection = db_write(MYSQL_config)
+
+    # get old_data
+    # with MYsqlConnection.connection.cursor() as cursor:
+    #     # cursor.execute(("select * FROM %s;" % configuration[parser.name]['mysql_tablename']))
+    #     cursor.execute("select * FROM DWD_Daten;")
+    #     record = cursor.fetchall()
+    #     names = [x[0] for x in cursor.description]
+    for key, value in parser_init.collect_data().items():
+        print('%s:\t%s' % (key, value))
+    # print(parser_init.collect_data())
+    # print(parser_init.collect_data())
+
+    # for index, key in enumerate(names):
+    #     if key == 'TIMESTAMP':
+    #         parsed_data[key] = [str(x[index]) for x in record]
+    #     else:
+    #         parsed_data[key] = [x[index] for x in record]
+    #
+    # logging_data = parser_init.SIM_class.simulate_values(parsed_data)
+    # MYsqlConnection.write_dict_data(logging_data)
+
+
+if __name__ == "__main__":
+    read_data()
