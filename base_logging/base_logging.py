a860b4579d25c83e7c62d708af32e4ee4828a7e7
Rumbelstilzchen <rumbelstilzchen@yahoo.de>
2023-01-28 14:01:42 +0100

    add stream logger for direct calls as of model training

--- base_logging/base_logging.py
+++ base_logging/base_logging.py
@@ -3,6 +3,25 @@
 import logging
 
 
+def set_stream_logger():
+    logging.root.setLevel(logging.INFO)
+    # create formatter and add it to the handler
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
+
+    for hdlr in logging.root.handlers:
+        logging.root.handlers.remove(hdlr)
+
+    # create console handler with a higher log level
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.DEBUG)
+    handler.setFormatter(formatter)
+    # add the handler to the logger
+    logging.root.addHandler(handler)
+
+    logger = logging.getLogger(__name__)
+    logger.info('Logging initialized')
+
+
 def set_logger(filename):
     logging.root.setLevel(logging.INFO)
     # create formatter and add it to the handler
